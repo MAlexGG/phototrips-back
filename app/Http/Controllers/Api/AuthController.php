63cf8aed6366ad31ca9c9124cb2dcbfb74567cdc
MAlexGG <alexandragalarzag@gmail.com>
2023-03-26 21:18:30 +0200

    logout and get all photos

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -32,8 +34,31 @@ class AuthController extends Controller
         ], 201);
     }
 
-    public function login()
+    public function login(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'res' => true,
+                'msg' => 'Las credenciales son incorrectas'
+            ]);
+        }
+        
+        $token = $user->createToken($request->email)->plainTextToken;
+
+        return response()->json([
+            'res' => true,
+            'msg' => 'Usuario identificado correctamente',
+            'token' => $token,
+            'user' => $user
+        ], 200);
+        
         
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PhotoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +21,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('/photos', [PhotoController::class, 'index']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
