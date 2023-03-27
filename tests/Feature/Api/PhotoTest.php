733df159edc23acd4e5524922a5007d17a23f843
MAlexGG <alexandragalarzag@gmail.com>
2023-03-27 14:20:42 +0200

    get all photos test

---
+++ tests/Feature/Api/PhotoTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use App\Models\Photo;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
+
+class PhotoTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+
+     use RefreshDatabase;
+
+    public function test_get_all_photos(): void
+    {
+        $user = User::factory()->create();
+
+        Auth::login($user);
+
+        Photo::factory()->create([
+            "user_id" => $user->id
+        ]);
+
+        Photo::all();
+
+        $response = $this->get('/api/photos');
+
+        $response->assertStatus(200)
+        ->assertJsonCount(1);
+    }
+}
